16fc9e1f92ce877f4812bcb08a6632d9101a1e10
St4rgaz3r <lefranccamille@hotmail.fr>
2020-08-13 19:59:23 +0200

    correct indent and README

--- src/app/card-game/card-game-generator.service.ts
+++ src/app/card-game/card-game-generator.service.ts
@@ -9,29 +9,29 @@ export class CardGameGeneratorService {
      cardDeck = new Array(52);
 
      public generateCardDeck() {
-       for (let i = 0; i < this.cardDeck.length; i++) {
-            let getRank = (i) => i % 13;
-            let getValue = (i) => getRank(i) + 1;
-            let getSuit = (i) => i / 13 | 0;
+          for (let i = 0; i < this.cardDeck.length; i++) {
+               let getRank = (i) => i % 13;
+               let getValue = (i) => getRank(i) + 1;
+               let getSuit = (i) => i / 13 | 0;
 
-            this.cardDeck[i] = i;
-       }
+               this.cardDeck[i] = i;
+          }
 
-       return this.cardDeck.map(this.getProperties);
+         return this.cardDeck.map(this.getProperties);
      }
 
      public getProperties(i) {
-       var rank = i % 13;
-       var value = rank + 1;
-       var cardColor = i / 13 | 0;
-       var cardHierarchy = i;
+          var rank = i % 13;
+          var value = rank + 1;
+          var cardColor = i / 13 | 0;
+          var cardHierarchy = i;
 
-       var ranksArray = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split(' ');
-       var cardColorsArray = '♦︎ ♥︎ ♠︎ ♣︎'.split(' ');
+          var ranksArray = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split(' ');
+          var cardColorsArray = '♦︎ ♥︎ ♠︎ ♣︎'.split(' ');
 
-       let readableNumber = ranksArray[rank];
-       let readableColor = cardColorsArray[cardColor];
+          let readableNumber = ranksArray[rank];
+          let readableColor = cardColorsArray[cardColor];
 
-       return { readableNumber, value, readableColor, cardHierarchy };
+          return { readableNumber, value, readableColor, cardHierarchy };
      }
 }
